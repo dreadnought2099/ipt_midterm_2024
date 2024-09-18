3cf8bf9134e4bef9962a9d633aec38fbac03657f
Loyd-894562 <catalanjhonlyodm@gmail.com>
2024-09-18 16:00:34 +0800

    added models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Order extends Model
 {
     use HasFactory;
 
-    // Category can have many products
+    // Order can have many products (many-to-many relationship)
     public function products()
     {
-        return $this->hasMany(Product::class);
+        return $this->belongsToMany(Product::class);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    // Product belongs to a category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Product belongs to many orders (many-to-many relationship)
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
 }
