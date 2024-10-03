b990edfa8ddb99488e2ca5317b1cb7ff5c65728c
dreadnought2099 <raymart.magallanes@bisu.edu.ph>
2024-10-03 10:08:35 +0800

    app.blade.php title change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Application</title>
+    <title>Shopping App</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
@@ -14,11 +14,11 @@
                 <li class="nav-item"><a class="nav-link" href="{{ route('categories.index') }}">Categories</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('products.index') }}">Products</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ route('orders.index') }}">Orders</a></li>
-                
+
             </ul>
         </div>
     </nav>
-    
+
     <div class="container mt-4">
         @yield('content')
     </div>
